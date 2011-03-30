087eca55c9978eab631d94e1f46a09f474e9ae4e
Petah <Petah.P@gmail.com>
2011-03-30 15:15:00 -0700

    Make SQL statement easier to read

--- upload/catalog/model/catalog/product.php
+++ upload/catalog/model/catalog/product.php
@@ -13,7 +13,21 @@ class ModelCatalogProduct extends Model {
 	}
 	
 	public function getProductsByCategoryId($category_id, $sort = 'pd.name', $order = 'ASC', $start = 0, $limit = 20) {
-		$sql = "SELECT *, pd.name AS name, p.image, m.name AS manufacturer, ss.name AS stock, (SELECT AVG(r.rating) FROM " . DB_PREFIX . "review r WHERE p.product_id = r.product_id GROUP BY r.product_id) AS rating FROM " . DB_PREFIX . "product p LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id) LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id) LEFT JOIN " . DB_PREFIX . "manufacturer m ON (p.manufacturer_id = m.manufacturer_id) LEFT JOIN " . DB_PREFIX . "stock_status ss ON (p.stock_status_id = ss.stock_status_id) LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id) WHERE p.status = '1' AND p.date_available <= NOW() AND pd.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p2s.store_id = '" . (int)$this->config->get('config_store_id') . "' AND ss.language_id = '" . (int)$this->config->get('config_language_id') . "' AND p2c.category_id = '" . (int)$category_id . "'";
+                $sql = "
+                    SELECT *, pd.name AS name, p.image, m.name AS manufacturer, ss.name AS stock, (SELECT AVG(r.rating)
+                    FROM " . DB_PREFIX . "review r
+                    WHERE p.product_id = r.product_id GROUP BY r.product_id) AS rating FROM " . DB_PREFIX . "product p
+                        LEFT JOIN " . DB_PREFIX . "product_description pd ON (p.product_id = pd.product_id)
+                        LEFT JOIN " . DB_PREFIX . "product_to_store p2s ON (p.product_id = p2s.product_id)
+                        LEFT JOIN " . DB_PREFIX . "manufacturer m ON (p.manufacturer_id = m.manufacturer_id)
+                        LEFT JOIN " . DB_PREFIX . "stock_status ss ON (p.stock_status_id = ss.stock_status_id)
+                        LEFT JOIN " . DB_PREFIX . "product_to_category p2c ON (p.product_id = p2c.product_id)
+                        WHERE p.status = '1'
+                            AND p.date_available <= NOW()
+                            AND pd.language_id = '" . (int) $this->config->get('config_language_id') . "'
+                            AND p2s.store_id = '" . (int) $this->config->get('config_store_id') . "'
+                            AND ss.language_id = '" . (int) $this->config->get('config_language_id') . "'
+                            AND p2c.category_id = '" . (int) $category_id . "'";
 		
 		$sort_data = array(
 			'pd.name',
